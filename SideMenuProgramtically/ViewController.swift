3761eef4b135dea3fd83409f8b57e74f018855a2
TalibIOS <goldsikkait13@gmail.com>
2023-03-04 16:05:37 +0530

    Initial Commit

---
+++ SideMenuProgramtically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuProgramtically
+//
+//  Created by Talib on 04/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
